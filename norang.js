c9afd9fa0c9725ee38834b8ddc427c078bc7167c
SoeunLee <iamsoeunlee@daum.net>
2021-09-22 12:06:48 +0900

    js

---
+++ norang.js
@@ -0,0 +1,33 @@
+  Kakao.init('ca399a68b8d6a1cae0df38eb8c324562');
+
+  function loginWithKakao() {
+    Kakao.Auth.login({
+      scope: 'friends,talk_message',
+      success: function(response) {
+        alert(JSON.stringify(response));
+      },
+      fail: function(err) {
+        alert(JSON.stringify(err));
+      }
+    });
+  }
+  function logout() {
+    if (!Kakao.Auth.getAccessToken()) {
+	  alert('Not logged in.');
+      return;
+    }
+    Kakao.Auth.logout(function() {
+	  alert('success');
+    });
+  }
+  function unlinkApp() {
+    Kakao.API.request({
+      url: '/v1/user/unlink',
+      success: function(res) {
+        alert('success: ' + JSON.stringify(res));
+      },
+      fail: function(err) {
+        alert('fail: ' + JSON.stringify(err));
+      },
+    });
+  }
